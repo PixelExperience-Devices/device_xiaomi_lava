5005679b3a6fdde60e8a4d0563464d2ca315ce37
Zinadin Zidan <zidan.roking@gmail.com>
2022-02-20 17:50:23 +0300

    lava: Use pe recovery as default recovery
    Change-Id: Ia41cd5f05c1e2de5875111fdcaabc93ac9556a74
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- aosp_lava.mk
+++ aosp_lava.mk
@@ -28,6 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_AOSP_RECOVERY := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 #
