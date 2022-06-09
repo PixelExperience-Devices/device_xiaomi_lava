0da77c2b43fd5adead0ba0bcd04682da46958b90
Vaisakh Murali <mvaisakh@statixos.com>
2022-06-09 14:14:08 +0300

    lava: Import SpeechVol Param for modifications
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Ibf408624613f9b62d35a8ce56113bc75fa885380
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/SpeechVol_AudioParam.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_param/SpeechVol_AudioParam.xml
+
 # NFC stack (AOSP)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_galahad/android.hardware.nfc.xml \
